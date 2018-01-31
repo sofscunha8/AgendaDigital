f52ba8dfcfa57794e899d668ac996c5e30e97fef
Sofia Cunha <sofiacunha.xb@hotmail.com>
2018-01-31 15:57:52 +0000

    no message

--- AgendaDigital/app/src/main/java/pt/ipp/estg/agendadigital/MainActivity.java
+++ AgendaDigital/app/src/main/java/pt/ipp/estg/agendadigital/MainActivity.java
@@ -1,13 +1,36 @@
 package pt.ipp.estg.agendadigital;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnLogout;
+    private Session session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        session = new Session(this);
+        if(!session.loggedin()){
+            logout();
+        }
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+    }
+
+    private void logout(){
+        session.setLoggedin(false);
+        finish();;
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
 }
